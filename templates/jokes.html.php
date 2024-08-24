d96d869f41802bd4e10bb7fce0aa7be99981feba
Todd Demone <tdemone1@gmail.com>
2024-08-24 10:15:07 -0400

    remove boilerplate from jokes template (it's now in layout template); include just the jokes-specific code

--- templates/jokes.html.php
+++ templates/jokes.html.php
@@ -1,24 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>List of Jokes</title>
-    <link rel="stylesheet" href="jokes.css">
-</head>
-<body>
-<?php if (isset ($error)): ?>
-<p>
-    <?= $error; ?>
-</p>
-<?php else: ?>
 <?php foreach ($jokes as $joke): ?>
 <blockquote>
-    <p>
-    <?= htmlspecialchars($joke, ENT_QUOTES, 'UTF-8'); ?>
-    </p>
+  <p>
+  <?= htmlspecialchars($joke, ENT_QUOTES, 'UTF-8'); ?>
+  </p>
 </blockquote>
 <?php endforeach; ?>
-<?php endif; ?>
-</body>
-</html>
