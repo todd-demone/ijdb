353e6bea1ea9ebfb91a4b4ba1b02aac85a4820f2
Todd Demone <tdemone1@gmail.com>
2024-08-24 10:21:08 -0400

    add ob_start() and ob_get_clean() so that the php in jokes template is executed but not sent to browser.

--- public/index.php
+++ public/index.php
@@ -1,51 +1,61 @@
 <?php
 
 try {
-    $pdo = new PDO('mysql:host=mysql;dbname=ijdb;charset=utf8mb4', 'ijdbuser', 'mypassword');
-    // $output = 'Database connection established.';
-    
-// CREATE TABLE EXAMPLE USING $pdo->exec()
+  $pdo = new PDO('mysql:host=mysql;dbname=ijdb;charset=utf8mb4', 'ijdbuser', 'mypassword');
+  // $output = 'Database connection established.';
 
-    // $sql = 'CREATE TABLE `joke` (
-    // id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    // joketext TEXT,
-    // jokedate DATE NOT NULL
-    // ) DEFAULT CHARACTER SET utf8 ENGINE=InnoDB';
+  // CREATE TABLE EXAMPLE USING $pdo->exec()
 
-    // $pdo->exec($sql);
+  // $sql = 'CREATE TABLE `joke` (
+  // id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  // joketext TEXT,
+  // jokedate DATE NOT NULL
+  // ) DEFAULT CHARACTER SET utf8 ENGINE=InnoDB';
 
-    // $output = 'Joke table successfully created.';
+  // $pdo->exec($sql);
 
-// UPDATE EXAMPLE USING $pdo->exec()
-    // $sql = 'UPDATE `joke` SET jokedate="2021-04-01"
-    //     WHERE joketext LIKE "%knock%"';
-    
-    // $affectedRows = $pdo->exec($sql);
+  // $output = 'Joke table successfully created.';
 
-    // $output = 'Updated ' . $affectedRows . ' rows.';
+  // UPDATE EXAMPLE USING $pdo->exec()
+  // $sql = 'UPDATE `joke` SET jokedate="2021-04-01"
+  //     WHERE joketext LIKE "%knock%"';
 
-// SELECT EXAMPLE USING $pdo->query()
-    $sql = 'SELECT `joketext` FROM `joke`';
-    // returns a PDOStatement object, which represents a result set 
-    // containing a list of rows returned from query
-    $result = $pdo->query($sql);
+  // $affectedRows = $pdo->exec($sql);
 
-    // fetch() method returns the next row as an associative array
-    // fetch() returns false when no more rows
-    // while ($row = $result->fetch()) {
-    //     $jokes[] = $row['joketext'];
-    // }
+  // $output = 'Updated ' . $affectedRows . ' rows.';
 
-    // PDOStatement objects behave just like arrays when passed to 
-    // foreach, so you can use this to extract the rows to an array
-    foreach ($result as $row) {
-        $jokes[] = $row['joketext'];
-    }
-    
-    $title = 'Joke list';
+  // SELECT EXAMPLE USING $pdo->query()
+  $sql = 'SELECT `joketext` FROM `joke`';
+  // returns a PDOStatement object, which represents a result set 
+  // containing a list of rows returned from query
+  $result = $pdo->query($sql);
 
-    include __DIR__ . '/../templates/jokes.html.php';
+  // fetch() method returns the next row as an associative array
+  // fetch() returns false when no more rows
+  while ($row = $result->fetch()) {
+    $jokes[] = $row['joketext'];
+  }
 
+  // PDOStatement objects behave just like arrays when passed to 
+  // foreach, so you can use this to extract the rows to an array
+  // foreach ($result as $row) {
+  //   $jokes[] = $row['joketext'];
+  // }
+
+  $title = 'Joke list';
+
+  ob_start();
+
+  // Include the template; The PHP code will be executed,
+  // but the resulting HTML will be stored in the buffer
+  // rather than sent to the browser.
+
+  include __DIR__ . '/../templates/jokes.html.php';
+
+  // Read the contents of the output buffer and store them
+  // in the $output variable for use in layout.html.php
+
+  $output = ob_get_clean();
 
 } catch(PDOException $e) {
     $output = 'Unable to connect to the database server.' . $e->getMessage() . 
