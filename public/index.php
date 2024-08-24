cd0d7e8e34a572fc0992d45c2329c1000af057d4
Todd Demone <tdemone1@gmail.com>
2024-08-24 09:30:54 -0400

    establish database connection using PDO and try-catch; output message re database using a template

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+try {
+    $pdo = new PDO('mysql:host=mysql;dbname=ijdb;charset=utf8mb4', 'ijdbuser', 'mypassword');
+    $output = 'Database connection established.';
+} catch(PDOException $e) {
+    $output = 'Unable to connect to the database server.' . $e->getMessage() . 
+    $e->getFile() . ':' . $e->getLine();
+}
+
+include __DIR__ . '/../templates/output.html.php';
