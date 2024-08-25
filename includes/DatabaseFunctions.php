9e278589ad3eef597c21cd186823b010d886862e
Todd Demone <tdemone1@gmail.com>
2024-08-25 12:06:46 -0400

    create functions helper file

---
+++ includes/DatabaseFunctions.php
@@ -0,0 +1,34 @@
+<?php
+function totalJokes($pdo) {
+  $stmt = $pdo->prepare('SELECT COUNT(*) FROM `joke`');
+  $stmt->execute();
+
+  $row = $stmt->fetch();
+
+  return $row[0];
+}
+
+function getJoke($pdo, $id) {
+  $stmt = $pdo->prepare('SELECT * FROM `joke` WHERE `id` = :id');
+  
+  $values = [
+    ':id' => $id
+  ];
+
+  $stmt->execute($values);
+
+  return $stmt->fetch();
+}
+
+function insertJoke($pdo, $joketext, $authorId) {
+  $stmt = $pdo->prepare('INSERT INTO `joke` (`joketext`, `jokedate`, `authorId`)
+    VALUES (:joketext, :jokedate, :authorId)');
+  
+  $values = [
+    ':joketext' => $joketext,
+    ':jokedate' => date('Y-m-d'),
+    ':authorId' => $authorId
+  ];
+
+  $stmt->execute($values);
+}
